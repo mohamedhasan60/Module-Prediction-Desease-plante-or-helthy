5b9a75dba5705e83e2ab78a20cc2fc7d5cccf49d
Mohamed Elgohary <107931438+mohamedhasan60@users.noreply.github.com>
2025-06-22 02:26:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def serve_image(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
